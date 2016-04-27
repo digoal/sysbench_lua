e6e2ab79e60123a59fb36a6890c0c1397853da9e
Digoal.zhou <digoal@126.com>
2016-04-27 22:43:34 +0800

    Update parallel_init_mysql.lua

--- lua/parallel_init_mysql.lua
+++ lua/parallel_init_mysql.lua
@@ -30,11 +30,23 @@ pathtest = string.match(test, "(.*/)") or ""
 dofile(pathtest .. "common.lua")
 
 function thread_init(thread_id)
+   local index_name
+   local i
    set_vars()
-   table_name = "sbtest" .. (thread_id+1)
-   print("Creating table " .. table_name)
+   
+   print("thread prepare"..thread_id)
 
-   query = [[
+   if (oltp_secondary) then
+     index_name = "KEY xid"
+   else
+     index_name = "PRIMARY KEY"
+   end
+
+   for i=thread_id+1, oltp_tables_count, num_threads  do
+     table_name = "sbtest" .. (thread_id+1)
+     print( "Creating table " .. table_name )
+
+     query = [[
 CREATE TABLE ]] .. table_name .. [[ (
 id INTEGER UNSIGNED NOT NULL ]] ..
 ((oltp_auto_inc and "AUTO_INCREMENT") or "") .. [[,
@@ -45,29 +57,28 @@ pad CHAR(60) DEFAULT '' NOT NULL,
 ) /*! ENGINE = ]] .. mysql_table_engine ..
 " MAX_ROWS = " .. myisam_max_rows .. " */"
 
-end
+     if (oltp_auto_inc) then
+       db_bulk_insert_init("INSERT INTO " .. table_name .. "(k, c, pad) VALUES")
+     else
+       db_bulk_insert_init("INSERT INTO " .. table_name .. "(id, k, c, pad) VALUES")
+     end
 
-
-function event(thread_id)
-   if (oltp_auto_inc) then
-     db_bulk_insert_init("INSERT INTO " .. table_name .. "(k, c, pad) VALUES")
-   else
-     db_bulk_insert_init("INSERT INTO " .. table_name .. "(id, k, c, pad) VALUES")
-   end
-
-   for j = 1,oltp_table_size do
-     c_val = sb_rand_str([[
+     for j = 1,oltp_table_size do
+       c_val = sb_rand_str([[
 ###########-###########-###########-###########-###########-###########-###########-###########-###########-###########]])
-     pad_val = sb_rand_str([[
+       pad_val = sb_rand_str([[
 ###########-###########-###########-###########-###########]])
 
-     if (oltp_auto_inc) then
-       db_bulk_insert_next("(" .. sb_rand(1, oltp_table_size) .. ", '".. c_val .."', '" .. pad_val .. "')")
-     else
-       db_bulk_insert_next("("..j.."," .. sb_rand(1, oltp_table_size) .. ",'".. c_val .."', '" .. pad_val .. "'  )")
+       if (oltp_auto_inc) then
+         db_bulk_insert_next("(" .. sb_rand(1, oltp_table_size) .. ", '".. c_val .."', '" .. pad_val .. "')")
+       else
+         db_bulk_insert_next("("..j.."," .. sb_rand(1, oltp_table_size) .. ",'".. c_val .."', '" .. pad_val .. "'  )")
+       end
      end
+     db_bulk_insert_done()
    end
+end
 
-db_bulk_insert_done()
+function event(thread_id)
 
 end
